4b65a096b0922c266ea1e0b8d780e0e72866f593
razop90 <45515008+razop90@users.noreply.github.com>
2018-12-01 07:01:07 -0800

    Upload project

---
+++ Tripit/Tripit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Tripit
+//
+//  Created by Raz Vaknin on 23 Kislev 5779.
+//  Copyright © 5779 razop. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Tripit/TripitTests/TripitTests.swift
@@ -0,0 +1,34 @@
+//
+//  TripitTests.swift
+//  TripitTests
+//
+//  Created by Raz Vaknin on 23 Kislev 5779.
+//  Copyright © 5779 razop. All rights reserved.
+//
+
+import XCTest
+@testable import Tripit
+
+class TripitTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
