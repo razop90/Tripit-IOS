abd85e97b230cd649d1e7afed7f3b19bc620dbde
Raz <razop90@gmail.com>
2018-12-01 07:17:38 -0800

    test

--- Tripit/Tripit/AppDelegate.swift
+++ Tripit/Tripit/AppDelegate.swift
@@ -13,7 +13,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     
-//sss
 
     func application(_application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
