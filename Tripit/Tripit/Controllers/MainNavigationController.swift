32c368475cdf19d21d5dc10577a26830d5c0057e
Alice <aliceka495@gmail.com>
2018-12-10 19:33:16 +0200

    Added Registration view+ a few changes at storyboad/Login view
    Registration: creation + Alerts.
    
    Storyboard: flow Changed, the first view should be regular and than a segue to login view, cuz a few users could be loged in before already, if not loged in , then it redirects to login view.
    
    Login: Secured password,placeholder.

--- Tripit/Tripit/Controllers/MainNavigationController.swift
+++ Tripit/Tripit/Controllers/MainNavigationController.swift
@@ -12,4 +12,62 @@ import UIKit
 
 class MainNavigationController : UINavigationController
 {
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var userPasswordTextField: UITextField!
+    @IBOutlet weak var repeatPasswordTextField: UITextField!
+    override func viewDidLoad(){
+        super.viewDidLoad()
+}
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        self.performSegue(withIdentifier: "loginView",sender:self)
+    }
+    @IBAction func registerButtonTapped(_ sender: Any) {
+        
+        let username = usernameTextField.text
+        let userPassword = userPasswordTextField.text
+        let userRepeatPassword = repeatPasswordTextField.text
+        
+        
+        //display alert messages
+        func displayMyAlertMessage(userMessage: String)
+        {
+            var myAlert = UIAlertController(title:"Alert", message:userMessage, preferredStyle: UIAlertController.Style.alert)
+            
+            let okAction = UIAlertAction(title: "OK", style: .default, handler: nil)
+            
+            myAlert.addAction(okAction)
+            
+            self.present(myAlert,animated: true, completion:nil)
+        }
+        
+        
+        //check for empty fields
+        if((username?.isEmpty)! || (userRepeatPassword?.isEmpty)! || (userPassword?.isEmpty)!)
+        {
+            //display alert
+            displayMyAlertMessage(userMessage: "All fields are required")
+            return
+            
+        }
+        //check if password match
+        if(userPassword != userRepeatPassword)
+        {
+            //display alert message
+            displayMyAlertMessage(userMessage: "Passwords do not match")
+            return
+        }
+        
+      
+       
+        //store data
+        
+      
+        
+    
+    }
 }
