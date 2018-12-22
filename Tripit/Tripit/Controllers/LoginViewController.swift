607f220912a2f1ed8d8ee811d2d40eb79b069740
Raz <razop90@gmail.com>
2018-12-22 21:18:00 +0200

    adding constraints and tapping events
    to login and registration views.
    improve design.
    
    Alice and Raz

--- Tripit/Tripit/Controllers/LoginViewController.swift
+++ Tripit/Tripit/Controllers/LoginViewController.swift
@@ -14,9 +14,18 @@ class LoginViewController : UIViewController {
     
     @IBOutlet weak var UsernameText: UITextField!
     @IBOutlet weak var PasswordText: UITextField!
-
+    @IBOutlet var viewcontainer: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let singleTap = UITapGestureRecognizer(target: self, action: #selector(LoginViewController.tapDetected))
+        viewcontainer.addGestureRecognizer(singleTap)
+    }
+    
+    @objc func tapDetected() {
+        self.UsernameText.endEditing(true)
+        self.PasswordText.endEditing(true)
     }
        
     @IBAction func onLoginTapped(_ sender: Any) {
@@ -31,7 +40,7 @@ class LoginViewController : UIViewController {
             Model.instance.signIn(email!, password!, { (res) in
                 
                 if(res) {
-                    self.loginSuccessfull();
+                    self.gotoMainview();
                 } else {
                     self.present(Consts.General.getCancelAlertController(title: "Login", messgae: "Failed while trying to Login. Please try again"), animated: true)
                 }
@@ -40,7 +49,12 @@ class LoginViewController : UIViewController {
         }
     }
   
-    func loginSuccessfull(){
+    @IBAction func OnRegisterTapped(_ sender: Any) {
+        UsernameText.text = ""
+        PasswordText.text = ""
+    }
+    
+    func gotoMainview(){
         //bundle is the place where all of the app's assets and source codes lived in before they compiled
         let mainStoryboard = UIStoryboard(name: "Main", bundle: Bundle.main)
         
@@ -49,7 +63,8 @@ class LoginViewController : UIViewController {
             return
         }
         //Navigate to the main view
-        present(mainNavigationVC, animated: true, completion: nil)    }
+        present(mainNavigationVC, animated: true, completion: nil)
+    }
     
     
   /*  private func navigateToMainInterface(){
