a9de2520ffe1b26b9ad6ebf86f9b0ae0e51c6ecd
Alice <aliceka495@gmail.com>
2019-01-04 22:20:07 +0200

    add buisy indicator in login/resister

--- Tripit/Tripit/Controllers/LoginViewController.swift
+++ Tripit/Tripit/Controllers/LoginViewController.swift
@@ -15,9 +15,13 @@ class LoginViewController : UIViewController {
     @IBOutlet weak var UsernameText: UITextField!
     @IBOutlet weak var PasswordText: UITextField!
     @IBOutlet var viewcontainer: UIView!
+    @IBOutlet var buisyIndicaitor: UIActivityIndicatorView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        self.buisyIndicaitor.isHidden = true
+        self.view.isUserInteractionEnabled = true
     }
     
     override func viewDidAppear(_ animated: Bool) {
@@ -45,15 +49,21 @@ class LoginViewController : UIViewController {
             present(Consts.General.getCancelAlertController(title: "Login", messgae: "Please enter Email or Password"), animated: true)
         }
         else{
+            buisyIndicaitor.startAnimating()
+            view.isUserInteractionEnabled = false
+            buisyIndicaitor.isHidden = false
             
             Model.instance.signIn(email!, password!, { (res) in
                 if(res) {
                     self.gotoMainview();
                 } else {
+                    self.buisyIndicaitor.stopAnimating()
+                    self.view.isUserInteractionEnabled = true
+                    self.buisyIndicaitor.isHidden = true
+                    
                     self.present(Consts.General.getCancelAlertController(title: "Login", messgae: "Failed while trying to Login. Please try again"), animated: true)
                 }
             })
-            
         }
     }
   
